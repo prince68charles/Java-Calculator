42d5c43584beb5d2478d1308fb63466ad6cc8d33
prince68charles <simonlavers3@outlook.com>
2025-09-11 11:30:53 -0300

    Started loop and input gathering

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,45 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
+
 public class Main {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
 
 
+        System.out.println("Enter the first number.\nType 0 to exit the program");
+        int num1 = scanner.nextInt();
+
+        if (num1 == 0) {
+
+            break;
+
+        }
+
+        else {
+
+
+            System.out.println("Please enter a operation.\n+, -, x, or ÷");
+            String opp = scanner.nextLine();
+
+           
+
+
+
+
+
+
+
+        }
+
+
+
+
+
+
+    }
 
 
 
