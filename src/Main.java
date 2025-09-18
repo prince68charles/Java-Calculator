9365eeee85bab2d7006c434e6160a389cdb64ae3
prince68charles <simonlavers3@outlook.com>
2025-09-17 21:00:15 -0300

    Submisson for assignment

--- src/Main.java
+++ src/Main.java
@@ -1,125 +1,101 @@
 import java.util.Scanner;
 
 public class Main {
-
-
-    public static void main (String[] args) {
-
+    public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
 
-
             System.out.println("Enter the first number.\nType 0 to exit the program");
-            int num1 = scanner.nextInt();
+            int num1 = scanner.nextInt(); // Input is type integer
 
             if (num1 == 0) {
-
                 break;
-
             }
+
 
             else {
 
-                System.out.println("Please enter a operation.\n+, -, x, or /");
+                System.out.println("Please enter an operation.\n+, -, *, /, s, or e");
                 String opp = scanner.next();
 
-                if (opp.equals("+") || opp.equals("-")  || opp.equals("x") || opp.equals("/") || opp.equals("e") || opp.equals("s")) {
+
+                if (opp.equals("+") || opp.equals("-") || opp.equals("*") || opp.equals("/") || opp.equals("e")) {
 
                     System.out.println("Enter a second number:");
                     int num2 = scanner.nextInt();
 
-
                     if (opp.equals("+")) {
-
                         System.out.println(num1 + num2);
 
-                    } else if (opp.equals("-")) {
+                    }
 
+                    else if (opp.equals("-")) {
                         System.out.println(num1 - num2);
 
+                    }
 
-                    } else if (opp.equals("x")) {
-
+                    else if (opp.equals("*")) {
                         System.out.println(num1 * num2);
 
-                    } else if (opp.equals("/")) {
+                    }
 
+                    else if (opp.equals("/")) {
                         System.out.println(num1 / num2);
 
+                    }
 
-                    } else if (opp.equals("e")) {
+                    else if (opp.equals("e")) {
 
-
-                        if ((num2 >= 0)) {
+                        if (num2 >= 0) {
 
                             int i = 0;
                             int exp = 1;
 
-
                             while (i < num2) {
-
-
                                 exp = exp * num1;
-
                                 i++;
                             }
+
                             System.out.println(exp);
-
-
-
 
                         }
 
                         else {
 
                             System.out.println("Error: Illegal exponent");
-
-
                         }
-
-
-
                     }
 
-                    else if (opp.equals("s")) {
-
-
-                        if (num2>0) {
-
-
-
-                            System.out.println(Math.sqrt((num1)));
-                        }
-
-                        else {
-
-
-                            System.out.println("Error: Negative Square Root");
-                        }
-
-                    }
 
                 }
+
+                else if (opp.equals("s")) {
+
+                    if (num1 > 0) {
+
+                        System.out.println(Math.sqrt(num1));
+                    }
+
+                    else {
+                        System.out.println("Error: Negative Square Root");
+                    }
+
+
+                }
+
 
                 else {
-
-
-                    System.out.println("Unknow operation");
-
+                    System.out.println("Unknown operation");
 
                 }
+
             }
-
-
-
-
 
 
         }
 
-
-
+        scanner.close();
     }
 }
